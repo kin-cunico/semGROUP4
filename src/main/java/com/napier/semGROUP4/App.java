738526128592e17428c7a53d8c498b3341345ae0
Kin Cunico <contact@kincunico.dev>
2025-10-06 11:38:30 +0200

    will be working on issues;

--- src/main/java/com/napier/semGROUP4/App.java
+++ src/main/java/com/napier/semGROUP4/App.java
@@ -2,6 +2,9 @@ package com.napier.semGROUP4;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Testing mvn");
+        System.out.println("Working on issues");
+
+        System.out.println("please pull from development branch and checkout to a new branch" +
+                "before working on this code base");
     }
 }
