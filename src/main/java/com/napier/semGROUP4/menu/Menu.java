db915776070305519a09580d5cb5a523697afacf
BrdEtr <jayrparkes@gmail.com>
2025-11-11 20:52:51 +0000

    Menu Fix

--- src/main/java/com/napier/semGROUP4/menu/Menu.java
+++ src/main/java/com/napier/semGROUP4/menu/Menu.java
@@ -4,16 +4,17 @@ import java.sql.Connection;
 import java.util.Scanner;
 import com.napier.semGROUP4.City;
 import com.napier.semGROUP4.CityService;
+import com.napier.semGROUP4.PopulationService;
 
 /**
  * Console menu for user interaction.
  */
 public class Menu {
 
-    private final PopulationService populationService;
+    private final PopulationService PopulationService;
 
     public Menu(Connection con) {
-        this.populationService = new PopulationService(con);
+        this.PopulationService = new PopulationService(con);
     }
 
     public void menuStart() {
@@ -32,8 +33,7 @@ public class Menu {
             int option = Integer.parseInt(scanner.nextLine());
 
             switch (option) {
-                case 1 -> cityReportMenu(scanner);
-                case 2 -> populationReportMenu(scanner);
+                case 1 -> populationReportMenu(scanner);
                 case 0 -> {
                     exit = true;
                     System.out.println("Exiting query menu...");
@@ -44,12 +44,12 @@ public class Menu {
         scanner.close();
     }
 
-    private void cityReportMenu(Scanner scanner) {
-        boolean cityExit = false;
+    private void populationReportMenu(Scanner scanner) {
+        boolean populationExit = false;
 
-        while (!cityExit) {
+        while (!populationExit) {
             System.out.println("""
-                    --- City Reports ---
+                    --- population Reports ---
                     1 - Get a single city by name
                     2 - List all cities in the world
                     3 - List cities in a continent
@@ -69,77 +69,13 @@ public class Menu {
 
             switch (choice) {
                 case 1 -> {
-                    System.out.print("Enter city name: ");
-                    String cityName = scanner.nextLine();
-                    City city = cityService.getCity(cityName);
-                    if (city != null)
-                        System.out.println(city);
-                    else
-                        System.out.println("City not found.");
+
                 }
 
-                case 2 -> cityService.displayCities(cityService.getAllCitiesInWorld());
-
-                case 3 -> {
-                    System.out.print("Enter continent name: ");
-                    cityService.displayCities(cityService.getCitiesInContinent(scanner.nextLine()));
-                }
-
-                case 4 -> {
-                    System.out.print("Enter region name: ");
-                    cityService.displayCities(cityService.getCitiesInRegion(scanner.nextLine()));
-                }
-
-                case 5 -> {
-                    System.out.print("Enter country name: ");
-                    cityService.displayCities(cityService.getCitiesInCountry(scanner.nextLine()));
-                }
-
-                case 6 -> {
-                    System.out.print("Enter district name: ");
-                    cityService.displayCities(cityService.getCitiesInDistrict(scanner.nextLine()));
-                }
-
-                case 7 -> {
-                    System.out.print("Enter number of top cities to show: ");
-                    int nWorld = Integer.parseInt(scanner.nextLine());
-                    cityService.displayCities(cityService.getTopNCitiesInWorld(nWorld));
-                }
-
-                case 8 -> {
-                    System.out.print("Enter continent name: ");
-                    String cont = scanner.nextLine();
-                    System.out.print("Enter number of top cities to show: ");
-                    int nCont = Integer.parseInt(scanner.nextLine());
-                    cityService.displayCities(cityService.getTopNCitiesInContinent(cont, nCont));
-                }
-
-                case 9 -> {
-                    System.out.print("Enter region name: ");
-                    String reg = scanner.nextLine();
-                    System.out.print("Enter number of top cities to show: ");
-                    int nReg = Integer.parseInt(scanner.nextLine());
-                    cityService.displayCities(cityService.getTopNCitiesInRegion(reg, nReg));
-                }
-
-                case 10 -> {
-                    System.out.print("Enter country name: ");
-                    String ctry = scanner.nextLine();
-                    System.out.print("Enter number of top cities to show: ");
-                    int nCtry = Integer.parseInt(scanner.nextLine());
-                    cityService.displayCities(cityService.getTopNCitiesInCountry(ctry, nCtry));
-                }
-
-                case 11 -> {
-                    System.out.print("Enter district name: ");
-                    String dist = scanner.nextLine();
-                    System.out.print("Enter number of top cities to show: ");
-                    int nDist = Integer.parseInt(scanner.nextLine());
-                    cityService.displayCities(cityService.getTopNCitiesInDistrict(dist, nDist));
-                }
+                case 2 -> {}
 
                 case 0 -> {
-                    cityExit = true;
+                    populationExit = true;
                     System.out.println("Exiting City Reports...");
                 }
 
